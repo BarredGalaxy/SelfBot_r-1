d8bc353673a94f87d1066af610bc04f753199a20
BarredGalaxy <DiscadeBOT@users.noreply.github.com>
2017-01-26 21:22:55 -0800

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -45,24 +45,23 @@ client.on('message', message => {
 client.on("message", msg => {
   const params = msg.content.split(" ").slice(1);
   if(msg.author !== client.user) return;
-  if (msg.content.startsWith(prefix + 'shutdown'))
-  if (msg.content.includes("trump")) 
-    msg.react('👎');
   if (msg.content.startsWith(prefix + "slap")) {
    let [username] = msg.content.split(" ").slice(1);
      msg.channel.sendMessage(username + ' was slapped by ' + msg.author)
- }
-   if (msg.content.startsWith(prefix + "offtopic")) {
+}
+  if (msg.content.startsWith(prefix + "offtopic")) {
   let [what, desc, pro2] = msg.content.split(" ").slice(1);
-    msg.edit(what + ", move to <#246872804664803329> pease!")
+    msg.delete()
+    msg.channel.sendMessage(what + ", move to <#246872804664803329> you skrub!")
 }
   if (msg.content.startsWith(prefix + "talk")) {
   let [what, desc, pro2] = msg.content.split("|").slice(1);
-    msg.edit("", {embed: {
+    msg.delete()
+    msg.channel.sendMessage("", {embed: {
             color: 2012323,
             author: {
                  name: what,
-                icon_url: ""
+                icon_url: pro2
             },
             description: desc,
             fields: [
@@ -71,7 +70,7 @@ client.on("message", msg => {
 }
   if (msg.content.startsWith(prefix + 'setgame'))
     msg.edit('```js\nclient.on("ready", () => {\n  client.user.setGame("Half-Life 3")\n});\n```')
-  if(msg.content.startsWith(prefix+"prune")) {
+  if(msg.content.startsWith(prefix + "prune")) {
     let messagecount = parseInt(params[0]);
     msg.channel.fetchMessages({limit: 100})
     .then(messages => {
@@ -88,4 +87,4 @@ client.on("message", msg => {
   }
 });
 
-client.login("nonononono")
+client.login("<no children>")
